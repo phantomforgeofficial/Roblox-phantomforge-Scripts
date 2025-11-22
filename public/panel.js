cc7059abf6e03b5a3907757d4da640a1f03824c6
phantomforgeofficial <phantom.forge.official@gmail.com>
2025-11-22 16:51:02 +0100

    Update panel.js

--- public/panel.js
+++ public/panel.js
@@ -2,13 +2,13 @@ const TOKEN = localStorage.getItem("session");
 
 if (!TOKEN) window.location.href = "index.html";
 
-const GITHUB_USER = "JOUW_GITHUB_NAAM";
-const REPO_NAME = "secure-script-system";
+const GITHUB_USER = "phantomforgeofficial";
+const REPO_NAME = "Roblox-phantomforge-Scripts";
 const FILE_PATH = "public/config.json";
 
 // ----------- LADEN -----------
 async function load() {
-  const r = await fetch(`https://jouw_github_naam.github.io/${REPO_NAME}/config.json`);
+  const r = const r = await fetch(`https://${GITHUB_USER}.github.io/${REPO_NAME}/public/config.json`);
   const json = await r.json();
 
   document.getElementById("places").value =
@@ -31,7 +31,7 @@ async function save() {
   await fetch(`https://api.github.com/repos/${GITHUB_USER}/${REPO_NAME}/contents/${FILE_PATH}`, {
     method: "PUT",
     headers: {
-      "Authorization": "token GITHUB_PERSONAL_TOKEN",
+      "Authorization": "token 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",
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
