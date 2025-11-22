261ee03c0171ce25fb2b8fea814ae1c1f31f87b7
phantomforgeofficial <phantom.forge.official@gmail.com>
2025-11-22 18:14:33 +0100

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,18 +1,4 @@
-// ------------------ LOGIN ------------------
-const VALID_USER = "Phantom Forge";
-const VALID_PASS = "IKHEETHARm";
 
-function login() {
-  const u = document.getElementById("user").value.trim();
-  const p = document.getElementById("pass").value.trim();
-
-  if (u === VALID_USER && p === VALID_PASS) {
-    localStorage.setItem("session", "ACTIVE");
-    window.location.href = "panel.html";
-  } else {
-    alert("Foute login!");
-  }
-}
 
 // ------------------ AUTH CHECK ------------------
 const TOKEN = localStorage.getItem("session");
