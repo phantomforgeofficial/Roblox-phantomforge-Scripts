3676c319122a1b1489c702d08e2bfe9d6346dc02
phantomforgeofficial <phantom.forge.official@gmail.com>
2025-11-24 01:13:22 +0100

    Update backend.js

--- public/backend.js
+++ public/backend.js
@@ -1,18 +1,8 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(req) {
-  const url = new URL(req.url);
-
+  const url = new URL(event.request.url);
   if (url.pathname === "/api/config") {
-    const config = await fetch(url.origin + "/config.json");
-    const text = await config.text();
-
-    return new Response(text, {
-      headers: { "Content-Type": "application/json" }
-    });
+    event.respondWith(fetch(url.origin + "/public/config.json"));
+    return;
   }
-
-  return new Response("Not found", { status: 404 });
-}
+  event.respondWith(new Response("Not found", { status: 404 }));
+});
